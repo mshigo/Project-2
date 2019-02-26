d1c95a0b719163874f03ddadd199b50ef5c5e7fb
Mike Shigo <mikeshigo@gmail.com>
2019-02-25 20:51:35 -0800

    API routes created and defined. Database isn't created yet however.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,85 @@
 var db = require("../models");
 
-module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+module.exports = function (app) {
+  // Get all events in database
+  app.get("/api/events", function (req, res) {
+    db.Event.findAll({})
+      .then(function (dbEvents) {
+        return res.json(dbEvents);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  //PUT route to update event by id
+  app.put("/api/event/:id", function (req, res) {
+    db.Event.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function (dbEvent) {
+        return res.json(dbEvent);
+      })
+      .catch(function (err) {
+        console.error(err);
+      })
+  });
+
+  // Get all events with certain breed
+  app.get("/api/events/:breed", function (req, res) {
+    db.Event.findAll({
+      where: {
+        breed: req.params.breed
+      }
+    })
+      .then(function (dbEvents) {
+        return res.json(dbEvents);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  });
+
+  // Get all events with certain category
+  app.get("/api/events/:category", function (req, res) {
+    db.Event.findAll({
+      where: {
+        category: req.params.category
+      }
+    })
+      .then(function (dbEvents) {
+        return res.json(dbEvents);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
+  });
+
+  // Create a new event
+  app.post("/api/new/event", function (req, res) {
+    db.Event.create(req.body)
+      .then(function (dbEvent) {
+        return res.json(dbEvent);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.delete("/api/event/:id", function (req, res) {
+    db.Event.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(function (dbEvent) {
+        return res.json(dbEvent);
+      })
+      .catch(function (err) {
+        console.error(err);
+      });
   });
 };
