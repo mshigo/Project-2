c9cbee5d01f488375d47bc9cbbbf0628c09db770
Daniel Homann <danielhomann4@gmail.com>
2019-03-04 20:28:18 -0800

    finished html and css

--- js/init.js
+++ js/init.js
@@ -4,5 +4,7 @@
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
+    $('.datepicker').datepicker();
+    $('.timepicker').timepicker();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
