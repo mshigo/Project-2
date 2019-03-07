5be8a01b8e94bd5919967d728df498577a19d81e
sam <sam.albasateneh@gmail.com>
2019-03-06 20:38:59 -0800

    new events js

---
+++ public/js/event.js
@@ -0,0 +1,88 @@
+$(document).ready(function() {
+    $("#submit").on('click', function (event) {
+        event.preventDefault();
+
+        var userInput = {
+            User: $('#userName').val().trim(),
+            PetName: $('#petName').val().trim(),
+            Breeds: $('#breed').val().trim(),
+            Location: $('#location').val().trim(),
+            Date: $('#date').val().trim(),
+            Time: $('#time').val().trim()
+
+
+        }
+        $.post("/api/posts/", userInput, function() {
+            window.location.href = "/events";
+          });
+        })
+        
+var eventContainer = $("#eventContainer");
+
+
+var events = [];
+function getEvents() {
+    $.get("/api/posts", function(data) {
+        events = data;
+        console.log(data);
+        initializeRows();
+    });
+}
+
+getEvents();
+
+function initializeRows() {
+    eventContainer.empty();
+    var newRows = [];
+    for (var i = 0; i < events.length; i++) {
+        newRows.push(createNewEvent(events[i]));
+    }
+    eventContainer.prepend(newRows);
+}
+
+
+
+function createNewEvent(userInput) {
+    var newInputRow = $(
+        [
+"<div class='card horizontal card hoverable'>",
+            "<div class='card-stacked'>",
+              "<div class='card-content'>",
+                "<h6> User Name: "  + userInput.User + "</h6>",
+                "<h6> Pet Name: " + userInput.PetName + "</h6>",
+                "<h6> Breeds: " + userInput.Breeds + "</h6>",
+                "<h6> Location: " + userInput.Location + "</h6>",
+                "<h6> Date: " + userInput.Date + "</h6>",
+                "<h6> Time: " + userInput.Time + "</h6>",
+              "</div>",
+              "<div class='card-action'>",
+                "<a href='/discussion'>Discussion Page</a>",
+              "</div>",
+            "</div>",
+          "</div>"
+
+        ].join("")
+    )
+    newInputRow.data("userInput", userInput);
+    return newInputRow;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
